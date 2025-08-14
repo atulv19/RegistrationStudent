e879830aea7b0889f3b66ba68c4621d501662bf6
atulv19 <av7583695@gmail.com>
2025-08-14 22:36:19 +0530

    first commit

---
+++ src/main/java/com/apidemo/controller/RegistrationController.java
@@ -0,0 +1,51 @@
+package com.apidemo.controller;
+
+import com.apidemo.entity.Registration;
+import com.apidemo.payload.RegistrationDto;
+import com.apidemo.service.RegistrationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/register")
+@CrossOrigin(origins = "*")
+public class RegistrationController {
+
+   private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    //http://localhost:8080/api/v1/register
+    @PostMapping
+    public ResponseEntity<String> CreateRegistration(@RequestBody  RegistrationDto registrationDto){
+        RegistrationDto registration = registrationService.createRegistration(registrationDto);
+        return new ResponseEntity<>("Create User", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteRegistration(@RequestParam long id){
+        registrationService.deleteRegistration(id);
+        return new ResponseEntity<>("Delete Registration",HttpStatus.OK);
+
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<String> updateRegistration(@PathVariable long id , @RequestBody RegistrationDto registrationDto){
+        registrationService.updateRegistration( id, registrationDto);
+        return new ResponseEntity<>("Update Registration",HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Registration>> getAllRegistration(){
+      List<Registration> registrations =   registrationService.getAllRegistration();
+      return new ResponseEntity<>(registrations,HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/apidemo/service/RegistrationService.java
@@ -0,0 +1,60 @@
+package com.apidemo.service;
+
+import com.apidemo.entity.Registration;
+import com.apidemo.payload.RegistrationDto;
+import com.apidemo.repository.RegistrationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RegistrationService {
+    private RegistrationRepository registrationRepository;
+
+    public RegistrationService(RegistrationRepository registrationRepository) {
+        this.registrationRepository = registrationRepository;
+    }
+    public RegistrationDto createRegistration(RegistrationDto registrationDto){
+        Registration registration1 = ConvertToEntity(registrationDto);
+        Registration save = registrationRepository.save(registration1);
+          return ConvertToDto(save);
+    }
+
+    Registration ConvertToEntity(RegistrationDto registrationDto){
+        Registration registration = new Registration();
+        registration.setName(registrationDto.getName());
+       registration.setEmail(registrationDto.getEmail());
+        registration.setMobile(registrationDto.getMobile());
+        return  registration;
+    }
+
+    RegistrationDto ConvertToDto(Registration registration){
+        RegistrationDto registrationDto = new RegistrationDto();
+        registrationDto.setName(registration.getName());
+        registrationDto.setEmail(registration.getEmail());
+        registrationDto.setMobile(registration.getMobile());
+        return registrationDto;
+    }
+
+    public void deleteRegistration(long id) {
+        registrationRepository.deleteById(id);
+    }
+
+    public void updateRegistration(long id, RegistrationDto registrationDto) {
+        Optional<Registration> byId = registrationRepository.findById(id);
+        if(byId.isPresent()){
+            Registration reg = byId.get();
+            reg.setName(registrationDto.getName());
+            reg.setEmail(registrationDto.getEmail());
+            reg.setMobile(registrationDto.getMobile());
+            registrationRepository.save(reg);
+        }
+
+    }
+
+    public List<Registration> getAllRegistration() {
+        List<Registration> registrations = registrationRepository.findAll();
+        return registrations;
+    }
+}
